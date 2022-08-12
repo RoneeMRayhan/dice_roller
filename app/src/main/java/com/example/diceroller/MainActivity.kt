acf74322ab104da51c754c64a94919ea82be142c
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2022-08-12 20:06:18 +0600

    Starter image added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,9 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
+
+        //Do a dice roll when the app starts
+        rollDice()
     }
 
     /**
